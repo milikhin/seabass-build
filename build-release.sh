ca44ee70a11caaccb93e30234cfe8868af1b0693
Mikhael Milikhin <mikhael@milikhin.name>
2016-09-08 12:09:58 +0800

    Update build-release.sh

--- build-release.sh
+++ build-release.sh
@@ -1,5 +1,5 @@
-APP_VERSION="0.2.9"
-APP_VVERSION="v0.2.9"
+APP_VERSION="0.2.10"
+APP_VVERSION="v0.2.10"
 N_STEPS=7
 CORDOVA_PPA_URL="http://ppa.launchpad.net/cordova-ubuntu/ppa/ubuntu"
 CORDOVA_PPA="ppa:cordova-ubuntu/ppa"
